1d80409be9996c72b75e4429ba285f245efdf425
Uzair <uzairmd073@gmail.com>
2023-01-28 22:03:02 +0530

    Project Completed

--- dsw/dsw-ecommerce-app/src/AuthContext/Data.jsx
+++ dsw/dsw-ecommerce-app/src/AuthContext/Data.jsx
@@ -48,304 +48,304 @@ email:""
 const [user,setuser] = useState(s)
 const [data,setdata] = useState([])
 const [name,setname] = useState(u)
-    let ndata= [
-      {
-          id: 1,
-          name: "adidas",
-          img:Adidas1,
-          price: 109.99,
-          size:1,
-          color:"black",
-          desc: "Alphabounce 1 Running Shoe - Men's"
-      },
-      {
-          id: 2,
-          name: "Puma",
-          price: 59.99,
-          size:1.3,
-          img: Puma,
-          desc: "Ever FS Sneaker - Men's"
-      },
-      {
-          id: 3,
-          name: "adidas",
-          price: 59.99,
-          size:1.4,
-          img: Adidas2,
-          color:"black",
-          desc: "Grand Court 2.0 Sneaker - Men's"
-      },
-      {
-          id: 4,
-          name: "adidas",
-          price: 79.99,
-          color:"white",
-          size:1.5,
-          img:Adidas3,
-          desc: "Lite Racer Adapt 5.0 Sneaker - Men's"
-      },
-      {
-          id: 5,
-          name: "Crown Vintage",
-          price: 79.99,
-          color:"white",
-          size:1.5,
-          img: crown,
-          desc: "Edsul Sneaker"
-      },
-      {
-          id: 6,
-          name: "New Balance",
-          price: 79.99,
-          color:"black",
-          size:2.4,
-          img: New_Balance,
-          desc: "Kaptir 2.0 Sneaker - Men's"
-      },
-      {
-          id: 7,
-          name: "adidas",
-          price: 109.99,
-          size:2.5,
+//     let ndata= [
+//       {
+//           id: 1,
+//           name: "adidas",
+//           img:Adidas1,
+//           price: 109.99,
+//           size:1,
+//           color:"black",
+//           desc: "Alphabounce 1 Running Shoe - Men's"
+//       },
+//       {
+//           id: 2,
+//           name: "Puma",
+//           price: 59.99,
+//           size:1.3,
+//           img: Puma,
+//           desc: "Ever FS Sneaker - Men's"
+//       },
+//       {
+//           id: 3,
+//           name: "adidas",
+//           price: 59.99,
+//           size:1.4,
+//           img: Adidas2,
+//           color:"black",
+//           desc: "Grand Court 2.0 Sneaker - Men's"
+//       },
+//       {
+//           id: 4,
+//           name: "adidas",
+//           price: 79.99,
+//           color:"white",
+//           size:1.5,
+//           img:Adidas3,
+//           desc: "Lite Racer Adapt 5.0 Sneaker - Men's"
+//       },
+//       {
+//           id: 5,
+//           name: "Crown Vintage",
+//           price: 79.99,
+//           color:"white",
+//           size:1.5,
+//           img: crown,
+//           desc: "Edsul Sneaker"
+//       },
+//       {
+//           id: 6,
+//           name: "New Balance",
+//           price: 79.99,
+//           color:"black",
+//           size:2.4,
+//           img: New_Balance,
+//           desc: "Kaptir 2.0 Sneaker - Men's"
+//       },
+//       {
+//           id: 7,
+//           name: "adidas",
+//           price: 109.99,
+//           size:2.5,
 
-          color:"blue",
-          img:Adidas4,
-          desc: "Fresh Foam Roav Sneaker - Men's"
-      },
-      {
-          id: 8,
-          name: "Puma",
-          price: 59.99,
-          color:"black",
-          size:2.6,
-          img: Puma2,
-          desc: "Softride One4All Sneaker - Men's"
-      },
-      {
-          id: 9,
-          name: "Puma",
-          price: 109.99,
-          color:"white",
-          size:2.7,
+//           color:"blue",
+//           img:Adidas4,
+//           desc: "Fresh Foam Roav Sneaker - Men's"
+//       },
+//       {
+//           id: 8,
+//           name: "Puma",
+//           price: 59.99,
+//           color:"black",
+//           size:2.6,
+//           img: Puma2,
+//           desc: "Softride One4All Sneaker - Men's"
+//       },
+//       {
+//           id: 9,
+//           name: "Puma",
+//           price: 109.99,
+//           color:"white",
+//           size:2.7,
 
-          img: Puma3,
-          desc: "Ever FS Sneaker"
-      },
-      {
-          id: 10,
-          name: "Crown Vintage",
-          price: 79.99,
-          color:"blue",
-          img: crown2,
-          size:2.9,
-          desc: "Ebben Sneaker"
-      },
-      {
-          id: 11,
-          name: "Vans",
-          price: 129.99,
-          color:"black",
-          size:3.4,
-          img: Vans5,
-          desc: "Runfalcon 2.0 Running Shoe - Men's"
-      },
-      {
-          id: 12,
-          name: "adidas",
-          price: 59.99,
-          size:3.5,
-          color:"white",
+//           img: Puma3,
+//           desc: "Ever FS Sneaker"
+//       },
+//       {
+//           id: 10,
+//           name: "Crown Vintage",
+//           price: 79.99,
+//           color:"blue",
+//           img: crown2,
+//           size:2.9,
+//           desc: "Ebben Sneaker"
+//       },
+//       {
+//           id: 11,
+//           name: "Vans",
+//           price: 129.99,
+//           color:"black",
+//           size:3.4,
+//           img: Vans5,
+//           desc: "Runfalcon 2.0 Running Shoe - Men's"
+//       },
+//       {
+//           id: 12,
+//           name: "adidas",
+//           price: 59.99,
+//           size:3.5,
+//           color:"white",
 
-          img:Adidas5,
-          desc: "Asher Slip-On Sneaker - Men's"
-      },
-      {
-          id: 13,
-          name: "Puma",
-          price: 109.99,
-          size:3.6,
-          color:"blue",
-          img: Puma4,
-          desc: "Ward Sneaker - Men's"
-      },
-      {
-          id: 14,
-          name: "Vans",
-          price: 79.99,
-          color:"black",
-          size:3.7,
-          img: Vans7,
-          desc: "Lite Racer BYD 2.0 Sneaker - Mens"
-      },
-      {
-          id: 15,
-          name: "Reebok",
-          price: 59.99,
-          color:"blue",
-          size:3.8,
-          img:Reebok2,
-          desc: "Hoops 3.0 Mid High-Top Sneaker - Men's"
-      },
-      {
-          id: 16,
-          name: "Vans",
-          price: 129.99,
-          color:"white",
-          size:3.9,
-          img: Vans8,
-          desc: "237 Sneaker - Men's"
-      },
-      {
-          id: 17,
-          name: "ASICS",
-          price: 109.99,
-          size:4.0,
-          color:"black",
-          img: ASICS,
-          desc: "Fresh Foam 680 v7 Running Shoe - Men's"
-      },
-      {
-          id: 18,
-          name: "ASICS",
-          price: 59.99,
-          color:"blue",
-          img:ASICS2,
-          size:4.5,
-          desc: "Run '70s Running Shoe - Men's"
-      },
-      {
-          id: 19,
-          name: "Vans",
-          price: 129.99,
-          color:"blue",
-          img: Vans5,
-          size:4.6,
-          desc: "GEL-Venture 8 Running Shoe - Men's"
-      },
-      {
-          id: 20,
-          name: "ASICS",
-          color:"black",
-          price: 109.99,
-          size:4.7,
-          img: ASICS3,
-          desc: "515 Sneaker - Men's"
-      },
-      {
-          id: 21,
-          name: "Reebok",
-          price: 79.99,
-          size:4.9,
-          img: Reebok4,
-          color:"blue",
-          desc: "Kaptir 2.0 Running Shoe - Men's"
-      },
-      {
-          id: 22,
-          name: "Vans",
-          price: 79.99,
-          color:"blue",
-          img: Vans7,
-          size:5.5,
-          desc: "840 v5 Running Shoe - Men's"
-      },
-      {
-          id: 23,
-          name: "Brooks",
-          color:"black",
-          price: 129.99,
-          img: Brooks3,
-          size:6.5,
-          desc: "GT-1000 11 Running Shoe - Men's"
-      },
-      {
-          id: 24,
-          name: "Reebok",
-          price: 79.99,
-          color:"blue",
-          img:Reebok5,
-          size:6.2,
-          desc: "Rebound Rugged Sneaker - Men's"
-      },
-      {
-          id: 25,
-          name: "Reebok",
-          price: 129.99,
-          img: Reebok3,
-          color:"black",
-          size:6.3,
-           desc: "Range High-Top Sneaker - Men's"
-      }, {
-          id: 26,
-          name: "ASICS",
-          price: 59.99,
-          size:6.4,
-          img: ASICS2,
-          color:"yellow",
+//           img:Adidas5,
+//           desc: "Asher Slip-On Sneaker - Men's"
+//       },
+//       {
+//           id: 13,
+//           name: "Puma",
+//           price: 109.99,
+//           size:3.6,
+//           color:"blue",
+//           img: Puma4,
+//           desc: "Ward Sneaker - Men's"
+//       },
+//       {
+//           id: 14,
+//           name: "Vans",
+//           price: 79.99,
+//           color:"black",
+//           size:3.7,
+//           img: Vans7,
+//           desc: "Lite Racer BYD 2.0 Sneaker - Mens"
+//       },
+//       {
+//           id: 15,
+//           name: "Reebok",
+//           price: 59.99,
+//           color:"blue",
+//           size:3.8,
+//           img:Reebok2,
+//           desc: "Hoops 3.0 Mid High-Top Sneaker - Men's"
+//       },
+//       {
+//           id: 16,
+//           name: "Vans",
+//           price: 129.99,
+//           color:"white",
+//           size:3.9,
+//           img: Vans8,
+//           desc: "237 Sneaker - Men's"
+//       },
+//       {
+//           id: 17,
+//           name: "ASICS",
+//           price: 109.99,
+//           size:4.0,
+//           color:"black",
+//           img: ASICS,
+//           desc: "Fresh Foam 680 v7 Running Shoe - Men's"
+//       },
+//       {
+//           id: 18,
+//           name: "ASICS",
+//           price: 59.99,
+//           color:"blue",
+//           img:ASICS2,
+//           size:4.5,
+//           desc: "Run '70s Running Shoe - Men's"
+//       },
+//       {
+//           id: 19,
+//           name: "Vans",
+//           price: 129.99,
+//           color:"blue",
+//           img: Vans5,
+//           size:4.6,
+//           desc: "GEL-Venture 8 Running Shoe - Men's"
+//       },
+//       {
+//           id: 20,
+//           name: "ASICS",
+//           color:"black",
+//           price: 109.99,
+//           size:4.7,
+//           img: ASICS3,
+//           desc: "515 Sneaker - Men's"
+//       },
+//       {
+//           id: 21,
+//           name: "Reebok",
+//           price: 79.99,
+//           size:4.9,
+//           img: Reebok4,
+//           color:"blue",
+//           desc: "Kaptir 2.0 Running Shoe - Men's"
+//       },
+//       {
+//           id: 22,
+//           name: "Vans",
+//           price: 79.99,
+//           color:"blue",
+//           img: Vans7,
+//           size:5.5,
+//           desc: "840 v5 Running Shoe - Men's"
+//       },
+//       {
+//           id: 23,
+//           name: "Brooks",
+//           color:"black",
+//           price: 129.99,
+//           img: Brooks3,
+//           size:6.5,
+//           desc: "GT-1000 11 Running Shoe - Men's"
+//       },
+//       {
+//           id: 24,
+//           name: "Reebok",
+//           price: 79.99,
+//           color:"blue",
+//           img:Reebok5,
+//           size:6.2,
+//           desc: "Rebound Rugged Sneaker - Men's"
+//       },
+//       {
+//           id: 25,
+//           name: "Reebok",
+//           price: 129.99,
+//           img: Reebok3,
+//           color:"black",
+//           size:6.3,
+//            desc: "Range High-Top Sneaker - Men's"
+//       }, {
+//           id: 26,
+//           name: "ASICS",
+//           price: 59.99,
+//           size:6.4,
+//           img: ASICS2,
+//           color:"yellow",
           
-          desc: "Pacer Future Street Plus Running Shoe - Men's"
-      },
-      {
-          id: 27,
-          name: "Brooks",
-          price: 109.99,
-          img: Brooks2,
-          size:6.6,
-          color:"blue",
-          desc: "Fresh Foam 680 v7 Running Shoe - Men's"
-      },
-      {
-          id: 28,
-          name: "Skechers",
-          price: 79.99,
-          color:"white",
-          size:6.7,
+//           desc: "Pacer Future Street Plus Running Shoe - Men's"
+//       },
+//       {
+//           id: 27,
+//           name: "Brooks",
+//           price: 109.99,
+//           img: Brooks2,
+//           size:6.6,
+//           color:"blue",
+//           desc: "Fresh Foam 680 v7 Running Shoe - Men's"
+//       },
+//       {
+//           id: 28,
+//           name: "Skechers",
+//           price: 79.99,
+//           color:"white",
+//           size:6.7,
 
-          img: New_Balance3,
-          desc: "Fresh Foam Roav Running Shoe - Men's"
-      },
-      {
-          id: 29,
-          name: "Skechers",
-          price: 79.99,
-          color:"black",
-          size:6.7,
-          img: Skechers,
-          desc: "Daily 3.0 Sneaker - Men's"
-      },
-      {
-          id: 30,
-          name: "Skechers",
-          price: 109.99,
-          size:6.7,
-          color:"yellow",
-          img: Skechers2,
-          desc: "Atwood Deluxe Sneaker - Men's"
-      },
-      {
-          id:31,
-          name:"Puma",
-          price:100.99,
-          color:"white",
-          size:6.8,
-          img:Puma3,
-          desc: "Atwood Deluxe Sneaker - Men's"
+//           img: New_Balance3,
+//           desc: "Fresh Foam Roav Running Shoe - Men's"
+//       },
+//       {
+//           id: 29,
+//           name: "Skechers",
+//           price: 79.99,
+//           color:"black",
+//           size:6.7,
+//           img: Skechers,
+//           desc: "Daily 3.0 Sneaker - Men's"
+//       },
+//       {
+//           id: 30,
+//           name: "Skechers",
+//           price: 109.99,
+//           size:6.7,
+//           color:"yellow",
+//           img: Skechers2,
+//           desc: "Atwood Deluxe Sneaker - Men's"
+//       },
+//       {
+//           id:31,
+//           name:"Puma",
+//           price:100.99,
+//           color:"white",
+//           size:6.8,
+//           img:Puma3,
+//           desc: "Atwood Deluxe Sneaker - Men's"
 
-      },
-      {
-          id:32,
-          name:"Skechers",
-          price:100.99,
-          size:6.8,
-          img:Skechers5,
-          color:"black",
-          desc: "Atwood Deluxe Sneaker - Men's"
+//       },
+//       {
+//           id:32,
+//           name:"Skechers",
+//           price:100.99,
+//           size:6.8,
+//           img:Skechers5,
+//           color:"black",
+//           desc: "Atwood Deluxe Sneaker - Men's"
 
-      }
-  ]
+//       }
+//   ]
 
 const [admin,setAdmin] = useState(false)
-  localStorage.setItem("Alldata",JSON.stringify(ndata))
+//   localStorage.setItem("Alldata",JSON.stringify(ndata))
 
   let arr = JSON.parse(localStorage.getItem("Alldata"))
   useEffect(()=>{
